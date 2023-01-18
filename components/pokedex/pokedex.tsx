acbe5944de7840d908a41dc73602c36db0373a54
Maurizio Guarini <maurizio.nicola.pio.guarini@pwc.com>
2023-01-18 12:22:27 +0100

    pokedex module and favicon

--- components/pokedex/pokedex.tsx
+++ components/pokedex/pokedex.tsx
@@ -1,9 +1,17 @@
 import { useState } from 'react';
+import styles from './pokedex.module.scss';
 
 const Pokedex = () => {
     return(
         <div>
-            pokedex component
+            <div className={styles.pokedex__container}>
+                <div className={styles.pokedex__image}>
+                    image box
+                </div>
+                <div className={styles.pokedex__details}>
+                    detail infos
+                </div>
+            </div>
         </div>
     );
 }
