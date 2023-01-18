417978b02a0a8ac4d6b7137a685845d0dbd7d56e
Maurizio Guarini <maurizio.nicola.pio.guarini@pwc.com>
2023-01-18 10:49:25 +0100

    pokedex component

---
+++ components/pokedex/pokedex.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+const Pokedex = () => {
+    return(
+        <div>
+            pokedex component
+        </div>
+    );
+}
+
+export default Pokedex;
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -2,6 +2,8 @@ import classNames from "classnames";
 import { Roboto } from "@next/font/google";
 import { InferGetStaticPropsType } from "next";
 
+import Pokedex from './../../components/pokedex/pokedex';
+
 const roboto = Roboto({
   weight: ["100", "300", "400", "500", "700", "900"],
   subsets: ["latin"],
@@ -11,6 +13,7 @@ export default function Home() {
   return (
     <div className={classNames({ [roboto.className]: true })}>
       Welcome to pokemon dash
+      <Pokedex></Pokedex>
     </div>
   );
 }
